15e23223bb1aee719f95b4af459fca3736278bee
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-19 14:15:45 -0700

    d

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,19 +2,18 @@
 #include "lists.h"
 
 /**
- *author stephen musyoka 
-* free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - Frees a linked list and its strings
+ * @head: Pointer to the list_t list to be freed
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+    list_t *current;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
-	}
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current->str);  // Free the string
+        free(current);       // Free the node
+    }
 }
