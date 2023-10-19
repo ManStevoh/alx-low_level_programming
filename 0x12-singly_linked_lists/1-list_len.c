698a89555e0d72028db9d9d5306f5f8d476b1ef0
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-19 13:59:46 -0700

    g

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,15 @@
 #include "lists.h"
 
 /**
- * Author Stephen musyoka 
-* list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - Returns the number of elements in a linked list (recursive version)
+ * @h: Pointer to the list_t list
  *
- * Return: number of elements in h
+ * Return: Number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+    if (h == NULL)
+        return 0;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
-	}
-	return (n);
+    return 1 + list_len(h->next);
 }
