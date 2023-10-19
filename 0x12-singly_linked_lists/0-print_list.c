4eb9cba0061cd1466ba46bc396994df5b54db553
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-19 13:25:59 -0700

    f

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+/*
+ * File: 0-print_list.c
+ * Author: [Your Name]
+ * Description: A function to print the elements of a singly linked list.
+ */
+
+#include <stdio.h>
+#include "listss.h"
+
+/**
+ * print_list - Prints all the elements of a linked list
+ * @h: Pointer to the list_t list to print
+ *
+ * Return: The number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+    size_t nodes_printed = 0;
+
+    while (h)
+    {
+        if (!h->str)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+
+        h = h->next;
+        nodes_printed++;
+    }
+
+    return (nodes_printed);
+}
