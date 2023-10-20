bcfecb65515d853fb88b2e854016765153e3366c
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-19 21:47:49 -0700

    y

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - This function is executed before the main function
+ * It prints a couple of lines to the standard output.
+ */
+void first(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
