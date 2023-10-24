751763bfdbc33cfff4b0bb773dd8163b409f1bdd
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-24 10:48:23 -0700

    gf

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,18 @@
-#include "lists.h"
+include "lists.h"
 
 /**
+ *data structures
  * lists_count - Returns the number of nodes in a linked list.
  * @head: A pointer to the head of the linked list.
- *
+ *Autho Stephen musyoka
  * Return: The number of nodes in the linked list.
  */
+
 size_t lists_count(const listint_t *head)
 {
     size_t count = 0;
 
-    while (head != NULL)
+    while (head)
     {
         count++;
         head = head->next;
