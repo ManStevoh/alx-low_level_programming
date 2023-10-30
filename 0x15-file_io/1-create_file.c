561a13376e1fea66241a9c10341818d9f8c9cddf
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-30 11:17:49 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int of, wf, lenth_memo = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (lenth_memo = 0; text_content[lenth_memo];)
+			lenth_memo++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	of = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wf = write(of, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (of == -1 || w == -1)
 		return (-1);
 
 	close(o);
