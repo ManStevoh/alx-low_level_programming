5d6ca4256a731abb50aa85f8c042e2340a5a7363
Stephen Musyoka <stephenmusyoka207gmail.com>
2023-10-30 12:24:56 -0700

    3cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,103 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fdb);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer_memory_alloc;
+
+	buffer_memory_alloc = malloc(sizeof(char) * 1024);
+
+	if (buffer_memory_alloc == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer_memory_alloc);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fdb)
+{
+	int cn;
+
+	cn = close(fdb);
+
+	if (cn == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdb);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int opn, ton, rf, wf;
+	char *buffer_memory_alloc;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer_memory_alloc = create_buffer(argv[2]);
+	opn = open(argv[1], O_RDONLY);
+	rf = read(opn, buffer_memory_alloc, 1024);
+	ton = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (opn == -1 || rf == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer_memory_alloc);
+			exit(98);
+		}
+
+		wf = write(ton, buffer_memory_alloc, rf);
+		if (ton == -1 || wf == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer_memory_alloc);
+			exit(99);
+		}
+
+		rf = read(opn, buffer_memory_alloc, 1024);
+		ton = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rf > 0);
+
+	free(buffer_memory_alloc);
+	close_file(opn);
+	close_file(ton);
+
+	return (0);
+}
